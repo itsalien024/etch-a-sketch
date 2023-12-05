3a2ce42108a8199b5ada91c80617c17acf4dce1c
alien <148527832+itsalien024@users.noreply.github.com>
2023-12-05 10:56:41 +0300

    Basic Functionality of choosing the grid size by the user

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,25 +1,60 @@
+let gridSize = document.querySelector("#gridSize");
+let setGridSize = document.querySelector("#setGridSize");
+let size;
+let cellSize;
+let gridBorder = 480;
+
+let cells = [];
+
 function makeGrid() {
   let container = document.querySelector("#container");
+  let isMouseDown = false;
 
-  for (let i = 0; i < 16; i++) {
+  container.innerHTML = '';
+
+  container.addEventListener("mousedown", function() {
+    isMouseDown = true;
+  });
+
+  container.addEventListener("mouseup", function() {
+    isMouseDown = false;
+  });
+
+  for (let i = 0; i < size; i++) {
     let row = document.createElement("div")
-    row.id = "row"
+    row.id = "row" + i;
     row.style.display = "flex";
-    row.style.border = 
     container.appendChild(row);
 
-    for (let x = 0; x < 16; x++) {
+    for (let x = 0; x < size; x++) {
       let cell = document.createElement("div")
-      cell.id = "cell";
-      cell.style.width = "30px";
-      cell.style.height = "30px";
-      cell.style.border = "red, 1px, solid";
+      cell.id = "cell" + i + "-" + x;
+      cell.style.width = cellSize + "px";
+      cell.style.height = cellSize + "px";
+      cell.addEventListener("mouseover", function() {
+        if (isMouseDown) {
+          cell.style.backgroundColor = "blue"
+        }
+      });
       row.appendChild(cell);
+      cells.push(cell);
     }
   }
 }
 
-makeGrid();
+setGridSize.addEventListener('click', function() {
+  size = Number(gridSize.value);
+  cellSize = gridBorder / size;
+  makeGrid();
+  return cellSize;
+});
 
 
 
+let clear = document.querySelector("#clear")
+
+clear.onclick = function() {
+  cells.forEach(cell => {
+    cell.style.backgroundColor = 'white';
+  })
+};
